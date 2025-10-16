4b2ad0946fa87c40376de6cee664102a8cfffc94
tarunaditya91 <shankertarunaditya369@gmail.com>
2025-10-16 22:15:43 +0530

    routing file added

--- EmployeeTask_Frontend/src/app/app.routes.ts
+++ EmployeeTask_Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { TaskList } from './components/task-list/task-list';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'employees', component: EmployeeList },
+  { path: 'tasks', component: TaskList },
+  { path: 'auth/login', component: Login },
+  { path: 'auth/register', component: Register },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' } // Wildcard for invalid routes
+];
